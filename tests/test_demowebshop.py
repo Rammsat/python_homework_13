cfafa2c31865d32cd20bc9f132bcb4b064ee2187
Rammsat <saturn9911@mail.ru>
2023-02-25 16:55:53 +0300

    Добавлены алюр степы для demoshop

--- tests/test_demowebshop.py
+++ tests/test_demowebshop.py
@@ -1,36 +1,49 @@
 from selene import have, be
 from selene.support.shared import browser
+from allure import step
 
 
 def test_login(demoshop):
-    demoshop.open('')
-    demoshop.element('.ico-logout').should(be.visible)
+    with step('Открыть сайт'):
+        demoshop.open('')
+    with step('Логин должен быть выполен'):
+        demoshop.element('.ico-logout').should(be.visible)
 
 
 def test_logout(demoshop):
-    demoshop.open('')
-    demoshop.element('.ico-logout').click()
-    demoshop.element('.ico-login').should(be.visible)
+    with step('Открыть сайт'):
+        demoshop.open('')
+    with step('Сделать лог аут'):
+        demoshop.element('.ico-logout').click()
+    with step('Кнопка для логина должна быть видна'):
+        demoshop.element('.ico-login').should(be.visible)
 
 
 def test_add_product_to_basket(demoshop):
-    demoshop.open('/build-your-cheap-own-computer')
-    demoshop.element('.add-to-cart-button').click()
+    with step('Открыть страницу товара'):
+        demoshop.open('/build-your-cheap-own-computer')
+    with step('Добавить товар в корзину'):
+        demoshop.element('.add-to-cart-button').click()
 
-    demoshop.element('.cart-qty').should(have.text('1'))
+    with step('Товар должен быть в корзине'):
+        demoshop.element('.cart-qty').should(have.text('1'))
 
 
 def test_add_to_compare_list(demoshop):
-    demoshop.open('/build-your-cheap-own-computer')
-    demoshop.element('.add-to-compare-list-button').click()
+    with step('Открыть страницу товара'):
+        demoshop.open('/build-your-cheap-own-computer')
+    with step('Добавить товар к сравнению'):
+        demoshop.element('.add-to-compare-list-button').click()
 
-    browser.element(".clear-list").should(be.visible)
+    with step('Кнопка для очистки списка сравнения должна быть видна'):
+        browser.element(".clear-list").should(be.visible)
 
 
 def test_search(demoshop):
-    demoshop.open('')
-    demoshop.element('.search-box-text').type('Simple Computer').press_enter()
+    with step('Открыть сайт'):
+        demoshop.open('')
+    with step('Выполнить поиск'):
+        demoshop.element('.search-box-text').type('Simple Computer').press_enter()
 
-    browser.element(".product-title").should(have.exact_text('Simple Computer'))
-
-
+    with step('Товар должен быть найден'):
+        browser.element(".product-title").should(have.exact_text('Simple Computer'))
